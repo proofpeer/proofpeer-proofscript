6f167d88af04341d98e1e33f64e37cea340451d2
Steven Obua <steven.obua@gmail.com>
2013-12-23 06:06:25 +0100

    Implemented parsing of first simple expressions.

--- src/main/scala/proofpeer/proofscript/compiler/Frontend.scala
+++ src/main/scala/proofpeer/proofscript/compiler/Frontend.scala
@@ -1,9 +1,181 @@
 package proofpeer.proofscript.compiler
 
-import proofpeer.indent._
+sealed trait ParseTree {
+  lazy val freeVars : Set[String] = calcFreeVars
+  protected def calcFreeVars : Set[String]   
+}
 
-object Frontend {
+object ParseTree {
   
+  sealed trait Expr extends ParseTree
+  
+  case class Integer(value : BigInt) extends Expr {
+    protected def calcFreeVars = Set()
+  }
+  case class Id(name : String) extends Expr {
+    protected def calcFreeVars = Set(name)
+  }
+  case class UnaryOperation(op : UnaryOperator, expr : Expr) extends Expr {
+    protected def calcFreeVars = expr.freeVars
+  }
+  case class BinaryOperation(op : BinaryOperator, left : Expr, right : Expr) extends Expr {
+    protected def calcFreeVars = left.freeVars ++ right.freeVars
+  }
+  case class CmpOperation(operators : Vector[CmpOperator], operands : Vector[Expr]) extends Expr {
+    protected def calcFreeVars = operands.foldLeft(Set[String]())((x, y) => x ++ y.freeVars)   
+  }
+  case class If(cond : Expr, thenCase : Expr, elseCase : Expr) extends Expr {
+    protected def calcFreeVars = cond.freeVars ++ thenCase.freeVars ++ elseCase.freeVars
+  }
+  case class App(f : Expr, args : List[Expr]) extends Expr {
+    protected def calcFreeVars = args.foldLeft(f.freeVars)(_ ++ _.freeVars)
+  }
+  case class Fun(params : List[String], body : Expr) extends Expr {
+    protected def calcFreeVars = body.freeVars -- params.toSet
+  }
+  case class Lazy(expr : Expr) extends Expr {
+    protected def calcFreeVars = expr.freeVars
+  }
+  
+  sealed trait Operator 
+ 
+  sealed trait UnaryOperator extends Operator 
+  case object Neg extends UnaryOperator
+  case object Not extends UnaryOperator
+
+  sealed trait BinaryOperator extends Operator
+  case object Add extends BinaryOperator
+  case object Sub extends BinaryOperator
+  case object Mul extends BinaryOperator
+  case object Div extends BinaryOperator
+  case object Mod extends BinaryOperator
+  
+  sealed trait CmpOperator extends Operator
+  case object Eq extends CmpOperator
+  case object NEq extends CmpOperator
+  case object Le extends CmpOperator
+  case object Leq extends CmpOperator
+  case object Gr extends CmpOperator
+  case object Geq extends CmpOperator  
+}
+
+object Parser {
+  
+import proofpeer.indent._
+import proofpeer.indent.API._
+import proofpeer.indent.APIConversions._
+
+def ltokenrule(nonterminal : String, c1 : Char, c2 : Char) : Grammar = 
+  tokenrule(nonterminal, Range.interval(c1, c2)) ++ lexical(nonterminal)
+  
+def ltokenrule(nonterminal : String, c : Char) : Grammar = ltokenrule(nonterminal, c, c)
+
+import ParseTree._
+
+val g_literals = 
+  ltokenrule("LowerLetter", 'a', 'z') ++
+  ltokenrule("UpperLetter", 'A', 'Z') ++
+  lexrule("Letter", "UpperLetter") ++
+  lexrule("Letter", "LowerLetter") ++  
+  ltokenrule("Underscore", '_') ++ 
+  ltokenrule("Plus", '+') ++  
+  ltokenrule("Minus", '-') ++
+  ltokenrule("Times", '*') ++
+  ltokenrule("Slash", '/') ++
+  ltokenrule("Le", '<') ++
+  ltokenrule("Gr", '>') ++
+  ltokenrule("Eq", '=') ++
+  ltokenrule("ExclamationMark", '!') ++
+  ltokenrule("QuestionMark", '?') ++
+  lexrule("Leq", "Le Eq") ++
+  lexrule("Geq", "Gr Eq") ++
+  lexrule("NEquals", "ExclamationMark Eq") ++
+  lexrule("Equals", "Eq_1 Eq_2") ++
+  ltokenrule("RoundBracketOpen", '(') ++
+  ltokenrule("RoundBracketClose", ')') ++  
+  ltokenrule("Digit", '0', '9') ++
+  lexrule("Nat", "Nat Digit") ++
+  lexrule("Nat", "Digit") ++
+  lexrule("Id", "Id Digit") ++
+  lexrule("Id", "Id Letter") ++
+  lexrule("Id", "Id Underscore Letter") ++
+  lexrule("Id", "Id Underscore Digit") ++
+  lexrule("Id", "LowerLetter") ++
+  lexrule("Val", literal("val")) ++
+  lexrule("Def", literal("def")) ++
+  lexrule("Mod", literal("mod")) 
+
+import Derivation.Context  
+  
+val g_expr =
+  rule("PrimitiveExpr", "Id", c => Id(c.Id.text)) ++
+  rule("Int", "Nat", c => Integer(BigInt(c.Nat.text, 10))) ++
+  rule("Int", "Minus Nat", c => Integer(-BigInt(c.Nat.text, 10))) ++
+  rule("PrimitiveExpr", "Nat", c => Integer(BigInt(c.Nat.text, 10))) ++
+  rule("PrimitiveExpr", "RoundBracketOpen Expr RoundBracketClose", c => c.Expr.result) ++
+  rule("CmpExpr", "CmpExpr CmpOp AddExpr", { c =>
+    val operator = c.CmpOp.resultAs[CmpOperator]
+    val operand = c.AddExpr.resultAs[Expr]
+    c.CmpExpr.resultAs[Expr] match {
+      case op : CmpOperation =>
+        CmpOperation(op.operators :+ operator, op.operands :+ operand)
+      case e =>
+        CmpOperation(Vector(operator), Vector(e, operand))
+    }
+  }) ++
+  rule("CmpExpr", "AddExpr", _.AddExpr.result) ++
+  rule("CmpOp", "Le", c => Le) ++
+  rule("CmpOp", "Gr", c => Gr) ++
+  rule("CmpOp", "Leq", c => Leq) ++
+  rule("CmpOp", "Geq", c => Geq) ++
+  rule("CmpOp", "Equals", c => Eq) ++
+  rule("CmpOp", "NEquals", c => NEq) ++
+  rule("AddExpr", "AddExpr Plus MultExpr", c => BinaryOperation(Add, c.AddExpr.resultAs[Expr], c.MultExpr.resultAs[Expr])) ++
+  rule("AddExpr", "AddExpr Minus MultExpr", c => BinaryOperation(Sub, c.AddExpr.resultAs[Expr], c.MultExpr.resultAs[Expr])) ++  
+  rule("AddExpr", "MultExpr", _.MultExpr.result) ++
+  rule("AddExpr", "Minus MultExpr", c => UnaryOperation(Neg, c.MultExpr.resultAs[Expr])) ++  
+  rule("MultExpr", "MultExpr Times BasicExpr", c => BinaryOperation(Mul, c.MultExpr.resultAs[Expr], c.BasicExpr.resultAs[Expr])) ++
+  rule("MultExpr", "MultExpr Slash BasicExpr", c => BinaryOperation(Div, c.MultExpr.resultAs[Expr], c.BasicExpr.resultAs[Expr])) ++
+  rule("MultExpr", "MultExpr Mod BasicExpr", c => BinaryOperation(Mod, c.MultExpr.resultAs[Expr], c.BasicExpr.resultAs[Expr])) ++
+  rule("MultExpr", "BasicExpr", _.BasicExpr.result) ++
+  rule("BasicExpr", "PrimitiveExpr", _.PrimitiveExpr.result) ++
+  rule("Expr", "CmpExpr", _.CmpExpr.result) 
+  
+val g_prog = 
+  g_literals ++
+  g_expr ++
+  rule("Prog", "Expr", c => c.Expr.result)
+ 
+  
+def parse(prog : String) {
+  if (!g_prog.info.wellformed) {
+    println("grammar errors:\n"+g_prog.info.errors)
+    return
+  }
+  println("term: '"+prog+"'")
+  val d = UnicodeDocument.fromString(prog)
+  val g = g_prog.parser.parse(d, "Prog", 0)
+  g match {
+    case None => 
+      println("Does not parse.")
+    case Some((v, i)) => 
+      if (v.isUnique && i == d.size) {
+        println("Parsed successfully.")
+        val result = Derivation.computeParseResult(g_prog, d, t => null, v)
+        println("Result:\n"+result.result)
+      } else if (i < d.size) {
+        println("Parse error at token "+i)
+      } else {
+        println("Ambiguous parse tree:\n"+Derivation.visualize(g_prog, v))       
+      }
+  }  
+  println()
+} 
+
+def main(args : Array[String]) {
+  parse("x - y == 10 < y <= z - 4")
+  parse("2 * (x + 4) + y")
+}
   
   
 }
