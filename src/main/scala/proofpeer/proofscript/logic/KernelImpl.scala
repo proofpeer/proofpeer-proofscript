d5532fcb97b389376ae7c8989c46ce1d13cc841a
Steven Obua <steven.obua@gmail.com>
2014-03-22 16:52:12 +0000

    Refactored independent utility code in KernelImpl into separate KernelUtils object.

--- src/main/scala/proofpeer/proofscript/logic/KernelImpl.scala
+++ src/main/scala/proofpeer/proofscript/logic/KernelImpl.scala
@@ -5,6 +5,7 @@ private class KernelImpl(val mk_theorem : (Context, Term) => Theorem) extends Ke
   import Term._
   import Type._
   import Utils._
+  import KernelUtils._
   
   sealed trait ContextKind 
   object ContextKind {
@@ -31,7 +32,7 @@ private class KernelImpl(val mk_theorem : (Context, Term) => Theorem) extends Ke
     def parentNamespaces = created.parentNamespaces
         
     def typeOfTerm(term : Term) : Option[Type] = 
-      KernelImpl.this.typeOfTerm(this, Map(), term)
+      KernelUtils.typeOfTerm(this, Map(), term)
     
     private def hasContextScope(name : Name) : Boolean = {
       name.namespace.isEmpty || name.namespace == Some(namespace)
@@ -249,10 +250,7 @@ private class KernelImpl(val mk_theorem : (Context, Term) => Theorem) extends Ke
       val ty = getTypeOfTerm(tm)
       mk_theorem(this, mk_equals(ty, tm, tm))
     }
-    
-    
 
-     
   }
   
   private var namespaces : Map[String, Context] = Map()
@@ -267,19 +265,7 @@ private class KernelImpl(val mk_theorem : (Context, Term) => Theorem) extends Ke
       case None => failwith("there is no completed namespace '" + namespace + "'")
     }
   }
-    
-  private def isQualifiedName(name : Name) : Boolean = name.namespace.isDefined
-  
-  private def isQualifiedTerm(term : Term) : Boolean = {
-    term match {
-      case PolyConst(name, alpha) => isQualifiedName(name)
-      case Const(name) => isQualifiedName(name)
-      case Comb(f, x) => isQualifiedTerm(f) && isQualifiedTerm(x)
-      case Abs(name, ty, body) => isQualifiedTerm(body)
-      case Var(name) => true
-    }
-  }
-      
+          
   def completeNamespace(context : Context) : Context = {
     if (!context.isInstanceOf[ContextImpl] || context.kernel != this) 
       failwith("context does not belong to this kernel")
@@ -323,32 +309,7 @@ private class KernelImpl(val mk_theorem : (Context, Term) => Theorem) extends Ke
         Map(),
         Map())
   }
-      
-  def typeOfTerm(c : Context, vars : Map[IndexedName, Type], term : Term) : Option[Type] = {
-    term match {
-      case PolyConst(name, alpha) =>
-        name match {
-          case Kernel.equals => Some(Fun(alpha, Fun(alpha, Prop)))
-          case Kernel.forall | Kernel.exists => Some(Fun(Fun(alpha, Prop), Prop))
-          case Kernel.choose => Some(Fun(Fun(alpha, Prop), alpha))            
-          case _ => None
-        }
-      case Const(name) => c.typeOfConst(name)
-      case Var(name) => vars.get(name)
-      case Comb(f, x) => 
-        (typeOfTerm(c, vars, f), typeOfTerm(c, vars, x)) match {
-          case (Some(Fun(fdomain, frange)), Some(xty)) if fdomain == xty =>
-            Some(frange)
-          case _ => None
-        }
-      case Abs(name, ty, body) =>
-        typeOfTerm(c, vars + (name -> ty), body) match {
-          case Some(bodyty) => Some(Fun(ty, bodyty))
-          case None => None            
-        }
-    }
-  }
-  
+        
   // This assumes that c1 and c2 belong to the same kernel and the same context
   private def findCommonAncestorContext(c1 : ContextImpl, c2 : ContextImpl) : ContextImpl = {
     var depth1 = c1.depth
@@ -371,127 +332,6 @@ private class KernelImpl(val mk_theorem : (Context, Term) => Theorem) extends Ke
       context1
   }
   
-  private def collectConsts(term : Term) : Set[Name] = collectConsts(term, Set())
-  
-  private def collectConsts(term : Term, consts : Set[Name]) : Set[Name] = {
-    term match {
-      case Const(name) => consts + name
-      case PolyConst(name, _) => consts + name
-      case Comb(f, g) => collectConsts(g, collectConsts(f, consts))
-      case Abs(_, _, body) => collectConsts(body, consts)
-      case Var(_) => consts
-    }
-  }
-  
-  private def mk_implies(hyp : Term, concl : Term) : Term = {
-    Comb(Comb(Const(Kernel.implies), hyp), concl)
-  }
-  
-  private def mk_equals(ty : Type, left : Term, right : Term) : Term = {
-    Comb(Comb(PolyConst(Kernel.equals, ty), left), right)    
-  }
-  
-  private def maxIndex(x : Option[Option[Integer]], y : Option[Option[Integer]]) : Option[Option[Integer]] = {
-    (x, y) match {
-      case (None, m) => m
-      case (m, None) => m
-      case (Some(x), Some(y)) =>
-        (x, y) match {
-          case (None, m) => Some(m)
-          case (m, None) => Some(m)
-          case (Some(x), Some(y)) => Some(Some(if (x > y) x else y))
-        }
-    }
-  }
-  
-  private def incIndex(x : Option[Option[Integer]]) : Option[Integer] = {
-    x match {
-      case None => None
-      case Some(None) => Some(1)
-      case Some(Some(x)) => Some(x + 1)
-    }
-  }
-  
-  private def findHighestVarIndex(name : String, term : Term) : Option[Option[Integer]] = {
-    term match {
-      case Const(_) => None
-      case PolyConst(_, _) => None
-      case Comb(f, g) => 
-        val fi = findHighestVarIndex(name, f)
-        val gi = findHighestVarIndex(name, g)
-        maxIndex(fi, gi)
-      case Abs(varname, _, body) =>
-        val vi = if (varname.name == name) Some(varname.index) else None
-        val bi = findHighestVarIndex(name, body)
-        maxIndex(vi, bi)
-      case Var(varname) =>
-        if (varname.name == name) Some(varname.index) else None
-    }
-  }
-  
-  // naive substitution, does not check for variable capture
-  private def substConst(term : Term, name : Name, substitute : Term) : Term = {
-    term match {
-      case Const(cname) if cname == name => substitute
-      case Comb(f, g) => Comb(substConst(f, name, substitute), substConst(g, name, substitute))
-      case Abs(x, ty, body) => Abs(x, ty, substConst(body, name, substitute))
-      case _ => term
-    }
-  }
-  
-  private def mk_abs(name : Name, ty : Type, prop : Term) : Term = {
-    val index = incIndex(findHighestVarIndex(name.name.name, prop))
-    val varname = IndexedName(name.name.name, index)
-    Abs(varname, ty, substConst(prop, name, Var(varname)))
-  }
-  
-  private def mk_forall(name : Name, ty : Type, prop : Term) : Term = {
-    Comb(PolyConst(Kernel.forall, ty), mk_abs(name, ty, prop))
-  }
-  
-  private def mk_exists(name : Name, ty : Type, prop : Term) : Term = {
-    Comb(PolyConst(Kernel.exists, ty), mk_abs(name, ty, prop))
-  }
-    
-  private def subst(term : Term, substitution : Map[IndexedName, Term], vars : Set[IndexedName]) : Term = {
-    term match {
-      case Var(varname) =>
-        substitution.get(varname) match {
-          case Some(t) => t
-          case None => term
-        }
-      case Comb(f, g) =>
-        Comb(subst(f, substitution, vars), subst(g, substitution, vars))
-      case Abs(varname, ty, body) =>
-        if (vars.contains(varname)) {
-          var name = varname.name
-          var index : Integer = if (varname.index.isDefined) varname.index.get + 1 else 1   
-          var newvarname : IndexedName = null
-          do {
-            newvarname = IndexedName(name, Some(index))
-            index = index + 1
-          } while (vars.contains(newvarname))
-          Abs(newvarname, ty, subst(body, substitution + (varname -> Var(newvarname)), vars + newvarname))  
-        } else {
-          Abs(varname, ty, subst(body, substitution, vars + varname))
-        }
-      case _ => term
-    }
-  } 
-  
-  private def freeVars(term : Term, bVars : Set[IndexedName], fVars : Set[IndexedName]) : Set[IndexedName] = {
-    term match {
-      case Var(varname) =>
-        if (bVars.contains(varname)) fVars else fVars + varname
-      case Comb(f, g) =>
-        freeVars(g, bVars, freeVars(f, bVars, fVars))
-      case Abs(varname, _, body) =>
-        freeVars(body, bVars + varname, fVars)
-      case _ => fVars
-    }    
-  }
-  
-  private def freeVars(term : Term) : Set[IndexedName] = freeVars(term, Set(), Set())
     
 }
 
---
+++ src/main/scala/proofpeer/proofscript/logic/KernelUtils.scala
@@ -0,0 +1,168 @@
+package proofpeer.proofscript.logic
+
+import Term._
+import Type._
+import Utils._
+
+object KernelUtils {
+
+  def isQualifiedName(name : Name) : Boolean = name.namespace.isDefined
+  
+  def isQualifiedTerm(term : Term) : Boolean = {
+    term match {
+      case PolyConst(name, alpha) => isQualifiedName(name)
+      case Const(name) => isQualifiedName(name)
+      case Comb(f, x) => isQualifiedTerm(f) && isQualifiedTerm(x)
+      case Abs(name, ty, body) => isQualifiedTerm(body)
+      case Var(name) => true
+    }
+  }
+  
+  def typeOfTerm(c : Context, vars : Map[IndexedName, Type], term : Term) : Option[Type] = {
+    term match {
+      case PolyConst(name, alpha) =>
+        name match {
+          case Kernel.equals => Some(Fun(alpha, Fun(alpha, Prop)))
+          case Kernel.forall | Kernel.exists => Some(Fun(Fun(alpha, Prop), Prop))
+          case Kernel.choose => Some(Fun(Fun(alpha, Prop), alpha))            
+          case _ => None
+        }
+      case Const(name) => c.typeOfConst(name)
+      case Var(name) => vars.get(name)
+      case Comb(f, x) => 
+        (typeOfTerm(c, vars, f), typeOfTerm(c, vars, x)) match {
+          case (Some(Fun(fdomain, frange)), Some(xty)) if fdomain == xty =>
+            Some(frange)
+          case _ => None
+        }
+      case Abs(name, ty, body) =>
+        typeOfTerm(c, vars + (name -> ty), body) match {
+          case Some(bodyty) => Some(Fun(ty, bodyty))
+          case None => None            
+        }
+    }
+  }
+
+  def collectConsts(term : Term) : Set[Name] = collectConsts(term, Set())
+  
+  def collectConsts(term : Term, consts : Set[Name]) : Set[Name] = {
+    term match {
+      case Const(name) => consts + name
+      case PolyConst(name, _) => consts + name
+      case Comb(f, g) => collectConsts(g, collectConsts(f, consts))
+      case Abs(_, _, body) => collectConsts(body, consts)
+      case Var(_) => consts
+    }
+  }
+  
+  def mk_implies(hyp : Term, concl : Term) : Term = {
+    Comb(Comb(Const(Kernel.implies), hyp), concl)
+  }
+  
+  def mk_equals(ty : Type, left : Term, right : Term) : Term = {
+    Comb(Comb(PolyConst(Kernel.equals, ty), left), right)    
+  }
+  
+  def maxIndex(x : Option[Option[Integer]], y : Option[Option[Integer]]) : Option[Option[Integer]] = {
+    (x, y) match {
+      case (None, m) => m
+      case (m, None) => m
+      case (Some(x), Some(y)) =>
+        (x, y) match {
+          case (None, m) => Some(m)
+          case (m, None) => Some(m)
+          case (Some(x), Some(y)) => Some(Some(if (x > y) x else y))
+        }
+    }
+  }
+  
+  def incIndex(x : Option[Option[Integer]]) : Option[Integer] = {
+    x match {
+      case None => None
+      case Some(None) => Some(1)
+      case Some(Some(x)) => Some(x + 1)
+    }
+  }
+  
+  def findHighestVarIndex(name : String, term : Term) : Option[Option[Integer]] = {
+    term match {
+      case Const(_) => None
+      case PolyConst(_, _) => None
+      case Comb(f, g) => 
+        val fi = findHighestVarIndex(name, f)
+        val gi = findHighestVarIndex(name, g)
+        maxIndex(fi, gi)
+      case Abs(varname, _, body) =>
+        val vi = if (varname.name == name) Some(varname.index) else None
+        val bi = findHighestVarIndex(name, body)
+        maxIndex(vi, bi)
+      case Var(varname) =>
+        if (varname.name == name) Some(varname.index) else None
+    }
+  }
+  
+  // naive substitution, does not check for variable capture
+  def substConst(term : Term, name : Name, substitute : Term) : Term = {
+    term match {
+      case Const(cname) if cname == name => substitute
+      case Comb(f, g) => Comb(substConst(f, name, substitute), substConst(g, name, substitute))
+      case Abs(x, ty, body) => Abs(x, ty, substConst(body, name, substitute))
+      case _ => term
+    }
+  }
+  
+  def mk_abs(name : Name, ty : Type, prop : Term) : Term = {
+    val index = incIndex(findHighestVarIndex(name.name.name, prop))
+    val varname = IndexedName(name.name.name, index)
+    Abs(varname, ty, substConst(prop, name, Var(varname)))
+  }
+  
+  def mk_forall(name : Name, ty : Type, prop : Term) : Term = {
+    Comb(PolyConst(Kernel.forall, ty), mk_abs(name, ty, prop))
+  }
+  
+  def mk_exists(name : Name, ty : Type, prop : Term) : Term = {
+    Comb(PolyConst(Kernel.exists, ty), mk_abs(name, ty, prop))
+  }
+    
+  def subst(term : Term, substitution : Map[IndexedName, Term], vars : Set[IndexedName]) : Term = {
+    term match {
+      case Var(varname) =>
+        substitution.get(varname) match {
+          case Some(t) => t
+          case None => term
+        }
+      case Comb(f, g) =>
+        Comb(subst(f, substitution, vars), subst(g, substitution, vars))
+      case Abs(varname, ty, body) =>
+        if (vars.contains(varname)) {
+          var name = varname.name
+          var index : Integer = if (varname.index.isDefined) varname.index.get + 1 else 1   
+          var newvarname : IndexedName = null
+          do {
+            newvarname = IndexedName(name, Some(index))
+            index = index + 1
+          } while (vars.contains(newvarname))
+          Abs(newvarname, ty, subst(body, substitution + (varname -> Var(newvarname)), vars + newvarname))  
+        } else {
+          Abs(varname, ty, subst(body, substitution, vars + varname))
+        }
+      case _ => term
+    }
+  } 
+  
+  def freeVars(term : Term, bVars : Set[IndexedName], fVars : Set[IndexedName]) : Set[IndexedName] = {
+    term match {
+      case Var(varname) =>
+        if (bVars.contains(varname)) fVars else fVars + varname
+      case Comb(f, g) =>
+        freeVars(g, bVars, freeVars(f, bVars, fVars))
+      case Abs(varname, _, body) =>
+        freeVars(body, bVars + varname, fVars)
+      case _ => fVars
+    }    
+  }
+  
+  def freeVars(term : Term) : Set[IndexedName] = freeVars(term, Set(), Set())
+    
+}
