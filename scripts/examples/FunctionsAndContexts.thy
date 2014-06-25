f2cfdb1810d7525267d4a4a90548ee3a041735a0
Steven Obua <steven.obua@gmail.com>
2014-06-26 00:43:16 +0100

    More Examples

---
+++ scripts/examples/FunctionsAndContexts.thy
@@ -0,0 +1,24 @@
+theory \examples\FunctionsAndContexts extends \root
+
+# Block A
+def 
+  dest t =
+    match t
+      case '‹p› ∧ ‹q›' => (1, p, q)
+      case '∃ X. ‹p› X ∧ ‹q› X' => (2, p, q)
+      case '‹p› = ‹q›' => (3, p, q)
+      case '∀ x. ‹p› x = ‹q› x' => (4, p, q)
+      case _ => nil  
+
+# Block B
+let 'x : 𝒰'
+let 'y : ℙ'
+choose inf: 'inf' infinity
+
+# Block C
+assert dest inf == [1, '∅ ∈ inf', '∀ x ∈ inf. x ∪ {x} ∈ inf']
+assert dest infinity == [2, 'X ↦ ∅ ∈ X', 'X ↦ ∀ x ∈ X. x ∪ {x} ∈ X']
+assert dest 'x = x' == [3, 'x', 'x']
+assert dest '∀ q. q = x' == [4, 'y ↦ y', 'y ↦ x']
+assert dest '∀ q. q = y' == [4, 'x : ℙ ↦ x', 'x : ℙ ↦ y']
+assert dest '∃ q. q = x' == nil
--- scripts/examples/Patterns.thy
+++ scripts/examples/Patterns.thy
@@ -59,3 +59,45 @@ assert brackets ["(", "(", "(", "(", ")", ")", ")", ")"]
 assert not brackets ["(", "(", ")"]
 
 
+# Let's do some term matching
+
+def 
+  polyAll '∀ x. x = x' = true
+  polyAll _ = false
+
+def monoAll t = t == '∀ x. x = x'
+
+val sUniv = '∀ x : 𝒰. x = x'
+val sProp = '∀ x : ℙ. x = x'
+
+assert polyAll sUniv and polyAll sProp
+assert monoAll sUniv
+assert not monoAll sProp
+
+let 'x : 𝒰'
+let 'y : ℙ'
+choose inf: 'inf' infinity
+
+def 
+  dest t =
+    match t
+      case '‹p› ∧ ‹q›' => (1, p, q)
+      case '∃ X. ‹p› X ∧ ‹q› X' => (2, p, q)
+      case '‹p› = ‹q›' => (3, p, q)
+      case '∀ x. ‹p› x = ‹q› x' => (4, p, q)
+      case _ => nil  
+
+assert dest inf == [1, '∅ ∈ inf', '∀ x ∈ inf. x ∪ {x} ∈ inf']
+assert dest infinity == [2, 'X ↦ ∅ ∈ X', 'X ↦ ∀ x ∈ X. x ∪ {x} ∈ X']
+assert dest 'x = x' == [3, 'x', 'x']
+assert dest '∀ q. q = x' == [4, 'y ↦ y', 'y ↦ x']
+assert dest '∀ q. q = y' == [4, 'x : ℙ ↦ x', 'x : ℙ ↦ y']
+assert dest '∃ q. q = x' == nil
+
+
+
+
+
+
+
+
