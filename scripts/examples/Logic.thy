0b71c24eb76c1d185dc657d5d43042fd4292f4a3
Steven Obua <steven.obua@gmail.com>
2014-06-27 18:36:38 +0100

    Added example for \root\transitive

--- scripts/examples/Logic.thy
+++ scripts/examples/Logic.thy
@@ -1,5 +1,6 @@
 theory \examples\Logic extends \root
 
+
 # term
   ------------------------------------
 
@@ -18,3 +19,26 @@ context
     t = assume 'x = y'
     assert term t == 'x = y'
   assert term t == '∀ y. x = y → x = y'
+
+
+# transitive
+  ------------------------------------
+
+context
+  let 'x1'
+  let 'x2'
+  let 'x3'
+  let 'x4'
+  assume t12: 'x1 = x2'
+  assume t23: 'x2 = x3'
+  assume t34: 'x3 = x4'
+  theorem t14: 'x1 = x4'
+    transitive (t12, t23, t34)
+  theorem 'x1 = x2'
+    transitive [t12]
+  failure transitive []
+  failure transitive t12
+
+# reflexive
+  ------------------------------------
+
