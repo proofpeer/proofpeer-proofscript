ce3c31da48517f9d06be1c10032a24176fa172ea
Steven Obua <steven.obua@gmail.com>
2014-06-27 11:34:00 +0100

    Added more logic statement examples.

--- scripts/examples/LogicStatements.thy
+++ scripts/examples/LogicStatements.thy
@@ -1,2 +1,54 @@
 theory \examples\LogicStatements extends \root
 
+# let introduction
+
+context
+  failure 'x'
+  let 'x'
+  assert 'x' == 'x : 𝒰'
+  failure 'x : ℙ'
+  failure
+    let 'x'
+
+context
+  failure 'x'
+  let x: 'x : ℙ'
+  assert x == 'x' == 'x : ℙ'
+  failure 'x : 𝒰'
+
+context
+  failure 'x'
+  val x = "x"
+  let x
+  assert 'x' == 'x : 𝒰'
+
+
+# let definition
+
+context
+  failure 'x'
+  let x_def: 'x = ∅ ∪ {∅}'
+  show x_def
+  assert 'x' == 'x : 𝒰'
+  assert term x_def == 'x = ∅ ∪ {∅}'
+
+context
+  failure 'x'
+  val x_def = 
+    let 'x = ∅ ∪ {∅}'
+  show x_def
+  assert 'x' == 'x : 𝒰'
+  assert term x_def == 'x = ∅ ∪ {∅}'
+
+context
+  failure 'x'
+  let x_def: 'x = true'
+  assert 'x' == 'x : ℙ'
+  assert term x_def == 'x = true'
+
+context
+  failure 'x'
+  failure
+    let 'x : 𝒰 = true'
+  let 'x : ℙ = true'
+
