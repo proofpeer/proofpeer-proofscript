fc8347f218300542db0934c32c1039a198d3ec0f
Steven Obua <steven.obua@gmail.com>
2014-09-19 02:52:03 +0100

    Fixes #37.

---
+++ scripts/issues/issue37.thy
@@ -0,0 +1,20 @@
+theory \issue37 extends \root
+
+assert "hello   
+  world" == "hello\n  world"
+
+val w = "hello     
+      
+    " 
+assert w == "hello\n\n    "
+assert size w == 11
+
+assert "a b" <> "ab"
+
+assert "  " <> ""
+assert size "  " == 2
+
+assert "a " <> "a"
+assert size "a " == 2
+
+assert "a " ++ " b" == "a  b"
