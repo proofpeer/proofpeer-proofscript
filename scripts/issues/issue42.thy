7b1f735896b75d9d67ef2c6ba1a97af6831db630
Steven Obua <steven.obua@gmail.com>
2014-10-08 11:59:44 +0100

    Introduced lift for explicit lifting, see also #42

---
+++ scripts/issues/issue42.thy
@@ -0,0 +1,16 @@
+theory issue42
+extends root
+
+theorem '∀x. x = x'
+  val (ctx,x,_) = destabs 'x ↦ x'
+  val foo
+  context <ctx>
+    foo = reflexive x
+  foo
+
+theorem '∀x. ⊤ = ⊤'
+  val (ctx,x,_) = destabs 'x ↦ ⊤'
+  val foo
+  context <ctx>
+    foo = reflexive '⊤'
+  lift (foo, true)
