8106acf624f8e9a8f94b69e54dee7140acf068fc
Phil Scott <me@philscotted.com>
2015-02-12 16:21:22 +0000

    Another refactor, to allow the user to specify whatever error type they want, and just have a coercion from KernelException.

--- src/main/scala/proofpeer/proofscript/logic/Conversion.scala
+++ src/main/scala/proofpeer/proofscript/logic/Conversion.scala
@@ -10,51 +10,49 @@ import EqualsInstances._
 
 /** Conversions are Kleisli arrows Term => Thy[E,Theorem]. The intended semantics of
     conversions is given by its `thenMonoid`. */
-// trait Conversions[F[_,_]] extends Theory[F] {
-//   // Nothing fancy for conversion exceptions. I might even demote this to Unit.
-//   def FSisME: MonadError[F,String]
-//   type ThyS[A] = Thy[String,A]
-//   implicit def ev = ThyIsMonadError(FSisME)
+trait Conversions[F[_,_],E] extends Theory[F,E] {
+  // Sticking to really boring errors for conversions for now.
+  def stringError: String => E
 
-//   type Conv = Kleisli[ThyS,Term,Thm]
-//   case class RichConv(k: Conv) {
-//     def apply(tm: Term): ThyS[Thm] = k.run(tm)
+  type Conv = Kleisli[ThyE,Term,Thm]
+  case class RichConv(k: Conv) {
+    def apply(tm: Term): ThyE[Thm] = k.run(tm)
 
-//     def destEquals(tm: Term) = dest_equals(tm) match {
-//       case Some((lhs,rhs,ty)) => (lhs,rhs,ty).point[ThyS]
-//       case None               =>
-//         ev.raiseError(tm + " is not an equality")
-//     }
-//     def andThenC(conv: RichConv): RichConv = kleisli[ThyS,Term,Thm](tm =>
-//       for (
-//         thm1            <- this(tm);
-//         eql             <- destEquals(thm1.proposition);
-//         (lhs,rhs,ty)    =  eql;
-//         thm2            <- conv(rhs);
-//         eql_            <- destEquals(thm2.proposition);
-//         (lhs_,rhs_,ty_) =  eql_;
-//         err <- if (lhs_ == rhs_) ().point[ThyS]
-//               else ev.raiseError[Unit]("andThen: " + lhs_ + " != " + rhs);
-//         thm <- bindF(transitive(thm1,thm2)) { fx  }
-//       )
-//       yield thm
-//     )
-//   }
+    def destEquals(tm: Term): ThyE[(Term,Term,Type)] = dest_equals(tm) match {
+      case Some((lhs,rhs,ty)) => (lhs,rhs,ty).point[ThyE]
+      case None               =>
+        ThyIsMonadError.raiseError(stringError(tm + " is not an equality"))
+    }
+    def andThenC(conv: RichConv): RichConv = kleisli[ThyE,Term,Thm](tm =>
+      for (
+        thm1            <- this(tm);
+        eql             <- destEquals(thm1.proposition);
+        (lhs,rhs,ty)    =  eql;
+        thm2            <- conv(rhs);
+        eql_            <- destEquals(thm2.proposition);
+        (lhs_,rhs_,ty_) =  eql_;
+        err <- if (lhs_ == rhs_) ().point[ThyE]
+               else ThyIsMonadError.raiseError[Unit](stringError(
+                      "andThen: " + lhs_ + " != " + rhs));
+        thm <- transitive(thm1,thm2)
+      )
+      yield thm
+    )
+  }
 
-//   implicit def kleisliIsRichConv(k: Kleisli[ThyS, Term, Thm]): RichConv =
-//     RichConv(k)
+  implicit def kleisliIsRichConv(k: Kleisli[ThyE, Term, Thm]): RichConv =
+    RichConv(k)
 
-// }
-//   object thenMonoid extends Monoid[Conv] {
-//     def zero = kleisli[ThyS,Term,Thm](tm => reflexive(tm))
-//     def append(x: Conv, y: => Conv) = (x andThenC y).run
+  object thenMonoid extends Monoid[Conv] {
+    def zero = kleisli[ThyE,Term,Thm](tm => reflexive(tm))
+    def append(x: Conv, y: => Conv) = (x andThenC y).k
 
-//     def ConvLaws {
-//       def thenZero[A](x: Conv, y: Conv, tm: Term)(implicit ev: IsEmpty[ThyS]):
-//           Boolean =
-//         ev.isEmpty((x andThenC zero)(tm))
-//       // TODO: Could add distributivity laws over the monoid (<+>) for the
-//       // Kleisli arrows.
-//     }
-//   }
-// }
+    def ConvLaws {
+      def thenZero[A](x: Conv, y: Conv, tm: Term)(implicit ev: IsEmpty[ThyE]):
+          Boolean =
+        ev.isEmpty((x andThenC zero)(tm))
+      // TODO: Could add distributivity laws over the monoid (<+>) for the
+      // Kleisli arrows.
+    }
+  }
+}
--- src/main/scala/proofpeer/proofscript/logic/Theory.scala
+++ src/main/scala/proofpeer/proofscript/logic/Theory.scala
@@ -10,23 +10,27 @@ import EqualsInstances._
 import Utils.KernelException
 
 /** `Theory` exposes inference rules without reference to contexts. The core type is
-  * `Theory.Thy`, whose interface is at least that of a MonadError capable of
-  *  throwing KernelException, plus versions of the kernel rules.
+  * `Theory.Thy`, whose interface is at least that of a MonadError, plus versions of
+  *  the kernel rules.
   *
   * @tparam `F` An instance of both [[scalaz.MonadError] for KernelException and
   * (when partially applied to KernelException) [[scalaz.Monad]].
+  * @tparam `E` The error type.
   */
-trait Theory[F[_,_]] {
+trait Theory[F[_,_],E] {
   def kernel: Kernel
-  def FKisME: MonadError[F,KernelException]
-  implicit def FKisM: Monad[({ type λ[A] = F[KernelException,A]})#λ]
+  def FEisME: MonadError[F,E]
+  def kernelError: KernelException => E
+
+  type FE[A]  = F[E,A]
+  implicit def FEisM: Monad[FE] = FEisME
 
   // Wrap theorems to limit access to contexts.
   class Thm private[Theory](private[Theory] val get: Theorem) {
     val proposition = get.proposition
   }
 
-  private def kleisli_ [E,A](f: Context => F[E,A]) =
+  private def kleisli_ [A](f: Context => F[E,A]) =
     Kleisli[({ type λ[A] = F[E,A] })#λ,Context,A](f)
 
   // Context is automatically threaded through computations (using Kleisli arrows
@@ -37,26 +41,17 @@ trait Theory[F[_,_]] {
     private[Theory] val k: Kleisli[({type λ[A] = F[E,A]})#λ,Context,A]) {
     def apply(ctx: Context): F[E,A] = k.run(ctx)
   }
+  type ThyE[A] = Thy[E,A]
 
-  implicit def ThyIsMonad[E](implicit ev: Monad[({type λ[A] = F[E,A]})#λ]) =
-    new ThyIsMonad[E] {
-      override def FEisM = ev
-    }
-  implicit def ThyIsMonadPlus[E](implicit ev: MonadPlus[({type λ[A] = F[E,A]})#λ]) =
-    new ThyIsMonadPlus[E] {
+  implicit object ThyIsMonad extends ThyIsMonad
+  object ThyIsMonadError extends ThyIsMonadError with ThyIsMonad
+  implicit def ThyIsMonadPlus(implicit ev: MonadPlus[({type λ[A] = F[E,A]})#λ]) =
+    new ThyIsMonadPlus {
       override def FEisMP = ev
-    }
-  implicit def ThyIsMonadError[E](implicit ev: MonadError[F,E]) =
-    new ThyIsMonadError[E] with ThyIsMonad[E] {
-      override def FEisME = ev
-      override def FEisM  = ev 
     }
 
   // Boiler plate makes me sad
-  trait ThyIsMonad[E] extends Monad[({type λ[A] = Thy[E,A]})#λ] {
-    implicit def FEisM: Monad[({ type λ[A] = F[E,A]})#λ]
-
-    private type FE[A] = F[E,A]
+  trait ThyIsMonad extends Monad[({type λ[A] = Thy[E,A]})#λ] {
     private type KE[A] = Kleisli[FE,Context,A]
     private implicit val ev: Monad[KE] = Kleisli.kleisliMonadReader
     import ev.monadSyntax._
@@ -64,31 +59,22 @@ trait Theory[F[_,_]] {
     override def bind[A,B](fa: Thy[E,A])(f: A => Thy[E,B]) =
       new Thy(fa.k >>= (f(_).k))
 
-    def liftF[E,A](x: F[E,A]): Thy[E,A] = new Thy(kleisli_ { _ => x })
+    def liftF[A](x: F[E,A]): Thy[E,A] = new Thy(kleisli_ { _ => x })
 
     def bindF[A,B](x: Thy[E,A])(f: F[E,A] => Thy[E,B]): Thy[E,B] =
       new Thy(kleisli_ { ctx => f(x.k.run(ctx)).k.run(ctx) })
-
-    def mapError[E2,A](x: Thy[E,A])(f: F[E,A] => F[E2,A]): Thy[E2,A] =
-        new Thy(kleisli_{ ctx: Context => f(x.k.run(ctx)) })
   }
-  trait ThyIsMonadPlus[E]
-      extends ThyIsMonad[E] with MonadPlus[({type λ[A] = Thy[E,A]})#λ] {
+  trait ThyIsMonadPlus
+      extends ThyIsMonad with MonadPlus[({type λ[A] = Thy[E,A]})#λ] {
     def FEisMP: MonadPlus[({ type λ[A] = F[E,A]})#λ]
 
-    override def FEisM = FEisMP
-    private type FE[A] = F[E,A]
     private type KE[A] = Kleisli[FE,Context,A]
     private implicit val ev: MonadPlus[KE] =
       Kleisli.kleisliMonadPlus[FE,Context](FEisMP)
     override def empty[A] = new Thy(ev.empty)
     override def plus[A](x: Thy[E,A], y: => Thy[E,A]) = new Thy(ev.plus(x.k,y.k))
   }
-  trait ThyIsMonadError[E] extends MonadError[Thy,E] {
-    def FEisME: MonadError[F,E]
-
-    private implicit def FEisM: Monad[({ type λ[A] = F[E,A]})#λ] = FEisME
-    private type FE[A]  = F[E,A]
+  trait ThyIsMonadError extends MonadError[Thy,E] {
     override def raiseError[A](e: E) =
       new Thy(FEisME.raiseError[A](e).liftKleisli[Context])
     override def handleError[A](fa: Thy[E,A])(handler: E => Thy[E,A]) =
@@ -109,47 +95,36 @@ trait Theory[F[_,_]] {
     }
    }
 
-  type ThyK[A] = Thy[KernelException,A]
-  implicit object ThyKInstances extends ThyKIsMonad with ThyKIsMonadError
-
-  trait ThyKIsMonad extends ThyIsMonad[KernelException] {
-    override def FEisM = FKisM
-  }
-  trait ThyKIsMonadError extends ThyKIsMonad with ThyIsMonadError[KernelException] {
-    override def FEisME = FKisME
-  }
-  trait ThyKIsMonadPlus extends ThyIsMonadPlus[KernelException]
-
   // Context access.
-  private def ask_ : Thy[KernelException,Context] =
-    new Thy(kleisli_(ctx => FKisM.point(ctx)))
+  private def ask_ : Thy[E,Context] =
+    new Thy(kleisli_(ctx => FEisM.point(ctx)))
 
-  private def scope_[A](ctx: Context)(fa: Thy[KernelException,A]) =
+  private def scope_[A](ctx: Context)(fa: Thy[E,A]) =
     new Thy(kleisli_(_ => fa.k.run(ctx)))
 
   // Put KernelExceptions into MonadError. Could skip this when specialising
   // MonadError on this exception type.
-  private def except[A](x: => A): Thy[KernelException,A] =
-    try x.point[ThyK]
+  private def except[A](x: => A): Thy[E,A] =
+    try x.point[ThyE]
     catch {
       case exc: Utils.KernelException =>
-        ThyKInstances.raiseError[A](exc)
+        ThyIsMonadError.raiseError[A](kernelError(exc))
     }
 
-  def typeOfConst[E](constName: Name): Thy[KernelException,Option[Type]] =
+  def typeOfConst(constName: Name): Thy[E,Option[Type]] =
     ask_.map(_.typeOfConst(constName))
 
-  def typeOfTerm(term: Term): Thy[KernelException,Type] =
+  def typeOfTerm(term: Term): Thy[E,Type] =
     ask_ >>= { _.typeOfTerm(term) match {
-      case Some(ty) => ty.point[ThyK]
-      case None     => ThyKInstances.raiseError[Type](
-        new KernelException("term not valid"))
+      case Some(ty) => ty.point[ThyE]
+      case None     => ThyIsMonadError.raiseError[Type](
+        kernelError(new KernelException("term not valid")))
     }}
 
   // NB: The following functions should not throw any exceptions concerning invalid
   // contexts.
-  def introducing(constName: Name, ty: Type)(mthm: Thy[KernelException,Thm]):
-      Thy[KernelException,Thm] =
+  def introducing(constName: Name, ty: Type)(mthm: Thy[E,Thm]):
+      Thy[E,Thm] =
     for (
       ctx    <- ask_;
       newCtx =  ctx.introduce(constName,ty);
@@ -157,7 +132,7 @@ trait Theory[F[_,_]] {
     )
     yield new Thm(ctx.lift(thm.get))
 
-  def assuming(tm: Term)(mthm: Thy[KernelException,Thm]): Thy[KernelException,Thm] =
+  def assuming(tm: Term)(mthm: Thy[E,Thm]): Thy[E,Thm] =
     for (
       ctx  <- ask_;
       asm  <- except(ctx.assume(tm));
@@ -165,8 +140,7 @@ trait Theory[F[_,_]] {
     )
     yield new Thm(ctx.lift(thm.get))
 
-  def defining(name: IndexedName, tm: Term)(mthm: Thy[KernelException,Theorem]):
-      Thy[KernelException,Thm] =
+  def defining(name: IndexedName, tm: Term)(mthm: Thy[E,Theorem]): Thy[E,Thm] =
     for (
       ctx  <- ask_;
       defn <- except(ctx.define(Name(None,name), tm));
@@ -174,8 +148,7 @@ trait Theory[F[_,_]] {
     )
     yield new Thm(ctx.lift(thm))
 
-  def choosing(name: IndexedName, tm: Term)(mthm: Thy[KernelException,Thm]):
-      Thy[KernelException,Thm] =
+  def choosing(name: IndexedName, tm: Term)(mthm: Thy[E,Thm]): Thy[E,Thm] =
     for (
       ctx  <- ask_;
       defn <- except(ctx.define(Name(None,name), tm));
@@ -183,19 +156,19 @@ trait Theory[F[_,_]] {
     )
     yield new Thm(ctx.lift(thm.get))
 
-  private def askThm(f: Context => Theorem): Thy[KernelException,Thm] =
+  private def askThm(f: Context => Theorem): Thy[E,Thm] =
     ask_ map { ctx => new Thm(f(ctx)) }
-  private def askThmExcept(f: Context => Theorem): Thy[KernelException,Thm] =
+  private def askThmExcept(f: Context => Theorem): Thy[E,Thm] =
     ask_ >>= { ctx => except(f(ctx)) map { thm => new Thm(thm) } }
 
-  def reflexive(x: Term): Thy[KernelException,Thm] = askThm(_.reflexive(x))
-  def beta(tm: Term): Thy[KernelException,Thm] = askThmExcept(_.beta(tm))
-  def eta(tm: Term): Thy[KernelException,Thm] = askThmExcept(_.eta(tm))
-  def normalize(tm: Term): Thy[KernelException,Thm] = askThm(_.normalize(tm))
+  def reflexive(x: Term): Thy[E,Thm] = askThm(_.reflexive(x))
+  def beta(tm: Term): Thy[E,Thm] = askThmExcept(_.beta(tm))
+  def eta(tm: Term): Thy[E,Thm] = askThmExcept(_.eta(tm))
+  def normalize(tm: Term): Thy[E,Thm] = askThm(_.normalize(tm))
   def mkFresh(name: IndexedName) = ask_ map (_.mkFresh(name))
-  def transitive(xy: Thm, yz: Thm): Thy[KernelException,Thm] =
+  def transitive(xy: Thm, yz: Thm): Thy[E,Thm] =
     askThmExcept(_.transitive(xy.get,yz.get))
-  def comb(fg: Thm, xy: Thm): Thy[KernelException,Thm] =
+  def comb(fg: Thm, xy: Thm): Thy[E,Thm] =
     askThmExcept(_.comb(fg.get,xy.get))
   def modusponens(p: Thm, qr: Thm) = askThmExcept(_.modusponens(p.get,qr.get))
   def abs(eq: Thm) = askThmExcept(_.abs(eq.get))
