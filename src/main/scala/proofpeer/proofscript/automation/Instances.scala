7695f1c1c76567b8d8826e7969bd57e74a42a2a1
Phil Scott <me@philscotted.com>
2015-04-08 17:42:02 +0100

    More instances for debugging.

--- src/main/scala/proofpeer/proofscript/automation/Instances.scala
+++ src/main/scala/proofpeer/proofscript/automation/Instances.scala
@@ -14,16 +14,30 @@ object KernelInstances {
     override def order(l: Namespace, r: Namespace) =
       (l.isAbsolute ?|? r.isAbsolute) |+| (l.components ?|? r.components)
   }
-  implicit object IndexedNameIsOrdered
-      extends Order[IndexedName]{
+  implicit object IndexedNameIsOrdered extends Order[IndexedName] {
     override def equal(l: IndexedName, r: IndexedName) = l == r
     override def order(l: IndexedName, r: IndexedName) = {
       (l.name ?|? r.name) |+| (l.index ?|? r.index)
     }
   }
+  // TODO: Temporarily poor show implementation
+  implicit object IndexedNameIsShow extends Show[IndexedName] {
+    override def show(n: IndexedName) = n.name.shows ++ n.index.shows
+  }
   implicit object NameIsOrdered extends Order[Name] {
     override def equal(l: Name, r: Name) = l == r
     override def order(l: Name, r: Name) =
       (l.namespace ?|? r.namespace) |+| (l.name ?|? r.name)
+  }
+  implicit object NameIsShow extends Show[Name] {
+    override def show(n: Name) = n.name.show
+  }
+  // TODO: Temporarily poor Term implementation, not even covering all cases.
+  implicit object TermIsShow extends Show[Term] {
+    override def shows(t: Term) =
+      t match {
+        case Term.PolyConst(name,_) => name.shows
+        case Term.Const(name)       => name.shows
+      }
   }
 }
