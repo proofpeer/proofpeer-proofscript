17b2069baa734d08bf594fca7b2b2929767b8d6e
Steven Obua <steven.obua@gmail.com>
2015-09-18 11:50:25 +0100

    Upgraded to scala-js 0.6.3

--- shared/src/main/scala/proofpeer/proofscript/proofdoc/ProofDoc.scala
+++ shared/src/main/scala/proofpeer/proofscript/proofdoc/ProofDoc.scala
@@ -6,6 +6,156 @@ sealed trait ProofDoc extends ParseTree
 
 object ProofDoc {
 
+  sealed trait Prop extends TracksSourcePosition
+  case class StringProp(s : String) extends Prop
+  case class ParseTreeProp(p : ParseTree) extends Prop
+
+  case class V(ty : T, keyword : Option[StringProp], props : Vector[Prop], values : Vector[V]) extends TracksSourcePosition {
+    def isWord = ty.isWord
+    def isBlock = ty.isBlock
+    def addValue(v : V) : V = V(ty, keyword, props, values :+ v)
+  }
+
+  val noprops : Vector[Prop] = Vector()
+  val novalues : Vector[V] = Vector()
+
+  sealed trait T {
+    def isWord : Boolean = true
+    def isBlock : Boolean = true
+  }
+  
+  sealed trait Word extends T {
+    override def isBlock = false
+  }
+  
+  sealed trait Block extends T {
+    override def isWord = false
+  }
+
+  /** A Paragraph contains only values and no props. Each of the values must be a word. */
+  case object Paragraph extends Block
+  
+  /** A Generator contains only of a single property which represents the code that is generated. */
+  case object Generator extends T 
+
+  /** A Text has only a single string property. */
+  case object Text extends Word
+
+  /** A FaultyText has only a single string property. */
+  case object FaultyText extends Word
+
+  /** Blocks represents a sequence of Block values. It has no props. */
+  case object Blocks extends Block
+
+  case object DummyBlock extends Block
+
+}
+
+class ProofDocSyntax(annotate : (Any, Option[proofpeer.indent.Span]) => Any) {
+
+  import proofpeer.indent._
+  import proofpeer.indent.regex._
+  import proofpeer.indent.Constraint._
+  import ProofDoc._
+
+  def range(cs : Char*) : Range = {
+    var r = Range()
+    for (c <- cs) r += Range(c)
+    r
+  }
+
+  def Subalign(a : String, b : String) = or(Indent(a, b), Align(a, b))
+  def SameOrNextLine(a : String, b : String) = Leq(FirstRow(b), LastRow(a), 1)
+  def DifferentLines(a : String, b : String) = Leq(LastRow(a), FirstRow(b), -1)
+  def OneLineApart(a : String, b : String) = Leq(LastRow(a), FirstRow(b), -2)
+
+  /** Generally useful regular expressions */
+  val letter = alt(chars('a', 'z'), chars('A', 'Z'))
+  val digit = chars('0', '9')
+  val anychar = CHAR(Range.universal)
+
+  var scope : String = null
+  var priority : Int = 1
+  var grammar : Grammar = Grammar()
+
+  def setScope(s : String) {
+    scope = s
+    priority = 1
+  }
+  def lex(terminal : String, r : RegularExpr) {
+    grammar = grammar ++ rule(terminal, r, Some(priority), scope)
+    priority += 1
+  }
+  def grule(nonterminal : String, rhs : String, constraint : Constraint, action : ParseContext => Any) {
+    def annotatedAction(c : ParseContext) : Any = {
+      val s : Option[Span] = if (c.span == null) None else Some(c.span)    
+      annotate(action(c), s)
+    }
+    grammar = grammar ++ rule(nonterminal, rhs, constraint, annotatedAction _)
+  }
+  def grule(nonterminal : String, rhs : String, action : ParseContext => Any) {
+    grule(nonterminal, rhs, unconstrained, action)
+  }
+  def annotateProp(p : Prop, s : Span) : Prop = {
+    annotate(p, Some(s)).asInstanceOf[Prop]
+  }
+  
+  /** Scope Paragraph */
+
+  setScope("Scope Paragraph")
+
+  val specialrange = range('*', '~', '$', '`', '@', '\\', '[', ']', '<', '>')
+  val simplechar = CHAR(-specialrange)
+  val escapechar = seq(char('\\'), CHAR(specialrange))
+  val simpleword = REPEAT1(alt(simplechar, escapechar)) 
+
+  lex("SIMPLEWORD", simpleword)
+  lex("ASTERISK", char('*'))
+  lex("OPENLINK", char('['))
+  lex("CLOSELINK", char(']'))
+  lex("OPENANGLE", char('<'))
+  lex("CLOSEANGLE", char('>'))
+  lex("USER", seq(char('@'), letter, REPEAT(alt(letter, digit))))
+  lex("KEYWORD", seq(REPEAT1(char('~')), REPEAT(letter), OPT(char('$'))))  
+  lex("REFERENCE", seq(char('<'), REPEAT(letter), char(':'), REPEAT(CHAR(-range('>'))), char('>')))
+    
+  /** Grammar rules */
+  grule("Blocks", "", c => V(Blocks, None, noprops, novalues))
+  grule("Blocks", "KBlocks", _.KBlocks[V])
+  grule("Blocks", "PBlocks", _.PBlocks[V])
+
+  // PBlocks always ends with a Paragraph, KBlocks always ends with a KBlock
+  grule("PBlocks", "Paragraph",
+    c => V(Blocks, None, noprops, Vector(c.Paragraph)))
+  grule("PBlocks", "KBlocks Paragraph", 
+    and(Align("KBlocks", "Paragraph"), DifferentLines("KBlocks", "Paragraph")), 
+    c => c.KBlocks[V].addValue(c.Paragraph))
+  grule("PBlocks", "PBlocks Paragraph", 
+    and(Align("PBlocks", "Paragraph"), OneLineApart("PBlocks", "Paragraph")), 
+    c => c.PBlocks[V].addValue(c.Paragraph))
+  grule("KBlocks", "Blocks KBlock",
+    and(Align("Blocks", "KBlock"), DifferentLines("Blocks", "KBlock")), 
+    c => c.Blocks[V].addValue(c.KBlock))
+
+  grule("Paragraph", "Word", 
+    c => V(Paragraph, None, noprops, Vector(c.Word)))
+  grule("Paragraph", "Paragraph Word", 
+    and(Subalign("Paragraph", "Word"), SameOrNextLine("Paragraph", "Word")),
+    c => c.Paragraph[V].addValue(c.Word))
+
+  // what are Words and KBlocks?
+
+  grule("Word", "SIMPLEWORD", 
+    c => V(Text, None, Vector(annotateProp(StringProp(c.text), c.span)), novalues))
+
+  grule("KBlock", "KEYWORD",
+    c => V(DummyBlock, None, noprops, novalues))
+}
+
+
+/*
+object ProofDoc {
+
   sealed trait Word extends ProofDoc {
     protected def calcVars = (Set(), Set())
   }
@@ -145,4 +295,4 @@ object ProofDoc {
     override def subBlocks = props
   }
 
-}
+} */
